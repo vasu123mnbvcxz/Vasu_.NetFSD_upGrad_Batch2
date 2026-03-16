1447ccde5bdc2d054df6aac302aee54049e7b155
vasu123mnbvcxz <148423794+vasu123mnbvcxz@users.noreply.github.com>
2026-03-17 00:01:06 +0530

    Add files via upload

---
+++ Week5/Day1/VehicleImplementation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week5Day1
+{
+    internal class VehicleImplementation
+    {
+        static void Main()
+        {
+            Car car = new Car("Toyota", 2000);
+            Bike bike = new Bike("Yamaha", 500);
+
+            Console.WriteLine("Car Rental: " + car.CalculateRental(3));
+            Console.WriteLine("Bike Rental: " + bike.CalculateRental(3));
+        }
+    }
+}
